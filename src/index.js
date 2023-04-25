3587af6f1e2999dba86480fa9e9c77bc6e3cb199
goYoungOd <lyslys77@tukorea.ac.kr>
2023-04-25 19:20:36 +0900

    tailwind 환경변수 설정2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./header/header.css";
+import "./global.css";
 
 ReactDOM.render(
   <React.StrictMode>
