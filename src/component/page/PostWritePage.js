4470a3012f2e6f72de14658e6c86c0d16ace8549
지윤 <dlwldbs369@tukorea.ac.kr>
2023-05-21 10:10:56 +0900

    fix : QnA 작성 및 보기
    write : db 필드와 변수 이름 매칭
    view : html tag -> 문자로 표시

--- src/component/page/PostWritePage.js
+++ src/component/page/PostWritePage.js
@@ -1,12 +1,13 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import TextInput from "../ui/TextInput";
 import Button from "../ui/Button";
 import Page from "./Page.css";
-import {CKEditor} from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import ReactHtmlParser from 'html-react-parser';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ReactHtmlParser from "html-react-parser";
+import axios from "axios";
 
 // onChange={(event, editor)=>{
 //     const data = editor.getData();
@@ -19,24 +20,24 @@ import ReactHtmlParser from 'html-react-parser';
 // }}
 
 const Wrapper = styled.div`
-    padding: 16px;
-    width: calc(100% - 50px);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  padding: 16px;
+  width: calc(100% - 50px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Container = styled.div`
-    width: 100%;
-    max-width: 720px;
-    margin-bottom: 30px;
+  width: 100%;
+  max-width: 720px;
+  margin-bottom: 30px;
 
-    & > * {
-        :not(:last-child){
-            margin-bottom: 16px;
-        }
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
     }
+  }
 `;
 const PostContainer = styled.div`
   padding: 8px 16px;
@@ -55,150 +56,155 @@ const ContentText = styled.p`
   white-space: pre-wrap;
 `;
 
-function PostWritePage(props){
-    
-    const navigate = useNavigate();
-    const styles = {
-        wrapper: {
-          margin: 8,
-          padding: 8,
-          display: "flex",
-          flexDirection: "row",
-          border: "1px solid grey",
-          borderRadius: 16,
-        },
-        imageContainer: {},
-        image: {
-          width: 50,
-          height: 48,
-          borderRadius: 25,
-          border: "1px solid grey",
-          float: "right",
-        },
-        contentContainer: {
-          marginLeft: 10,
-          display: "flex",
-          flexDirections: "column",
-          float: "right",
-          marginRight: 14,
-        },
-        nameText: {
-          color: "black",
-          fontSize: 16,
-          fontWeight: "bold"
-        },
-        commentText: {
-          color: "black",
-          fontSize: 16,
-        },
-        Problem: {
-          float: "left",
-          fontSize: 16,
-        },
-    };
-    // const [postTitle, setpostTitle] = useState({
-    //     postTitle: ``,
-    //     postContent: ``
-    // });
-    const [postContent, setpostContent] = useState({
-        Title: '',
-        Content: ''
+function PostWritePage(props) {
+  const navigate = useNavigate();
+  const styles = {
+    wrapper: {
+      margin: 8,
+      padding: 8,
+      display: "flex",
+      flexDirection: "row",
+      border: "1px solid grey",
+      borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+      width: 50,
+      height: 48,
+      borderRadius: 25,
+      border: "1px solid grey",
+      float: "right",
+    },
+    contentContainer: {
+      marginLeft: 10,
+      display: "flex",
+      flexDirections: "column",
+      float: "right",
+      marginRight: 14,
+    },
+    nameText: {
+      color: "black",
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    commentText: {
+      color: "black",
+      fontSize: 16,
+    },
+    Problem: {
+      float: "left",
+      fontSize: 16,
+    },
+  };
+  // const [postTitle, setpostTitle] = useState({
+  //     postTitle: ``,
+  //     postContent: ``
+  // });
+  const [postContent, setpostContent] = useState({
+    title: "",
+    question: "",
+  });
+
+  const test = () => {
+    axios
+      .post("http://127.0.0.1:8000/api/v1/qna/questions/", {
+        title: postContent.title,
+        question: postContent.question,
+      })
+      .then(function (response) {
+        console.log(response);
+        // setDataList(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // const [postTitle, postContent] = useState({
+
+  // })
+  //적힌 내용들 저장해주는 state => 화면에 보여주려고!
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setpostContent({
+      ...postContent,
+      [name]: value,
     });
+    console.log(postContent);
+  };
 
-    // const [postTitle, postContent] = useState({
-       
-    // })
-    //적힌 내용들 저장해주는 state => 화면에 보여주려고!
-    const [viewContent, setViewContent] = useState([]);
-
-
-    const getValue = e => {
-        const {name, value} = e.target;
-        setpostContent({
-            ...postContent,
-            [name]: value
-        })
-        console.log(postContent);
-        
-    }
-
-    return(
-        <Wrapper>
-            <Container>
-              <Button
+  return (
+    <Wrapper>
+      <Container>
+        <Button
           title="뒤로 가기"
           onClick={() => {
             navigate("/qna");
           }}
         />
-            </Container>
-            <Container classname="post-view">
-            <input style={{height: 50}} 
-                className="title-input" 
-                    type='text'
-                placeholder='  제목'
-                    onChange={getValue}
-                name="title"
-                />
-            <CKEditor
-                editor={ClassicEditor}
-                // data="여기 입력해줘여"
-                onReady={editor =>{
-                        }}
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    console.log({event, editor, data});
-                    setpostContent({
-                        ...postContent,
-                        content: data
-                    })
-                    console.log(postContent);
-                }}
-                onBlur={(event, editor) => {
-                    console.log('Blur.', editor);
-                }}
-                onFocus={(event, editor) => {
-                    console.log('Focus.', editor);
-                }}
-            />
-             <button
-                className="submit-button"
-                onClick={()=>{
-                    setViewContent(viewContent.concat({...postContent}));
-                }} >
-                    <Button title="글 작성하기"/>
-                </button>  
-                </Container>
-            <div></div>
-                <Container>
-                
-                </Container>
-            <Container>
-                <PostContainer className="post-container">
+      </Container>
+      <Container classname="post-view">
+        <input
+          style={{ height: 50 }}
+          className="title-input"
+          type="text"
+          placeholder="  제목"
+          onChange={getValue}
+          name="title"
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          // data="여기 입력해줘여"
+          onReady={(editor) => {}}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setpostContent({
+              ...postContent,
+              question: data,
+            });
+            console.log(postContent);
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+        <button className="submit-button" onClick={test}>
+          <Button title="글 작성하기" />
+        </button>
+      </Container>
+      <div></div>
+      <Container></Container>
+      <Container>
+        <PostContainer className="post-container">
+          <div>
+            {/* <img src="./public/profile.png" style={styles.image}></img> */}
+
+            {/* <TitleText>{state.title}</TitleText> */}
+            <TitleText>
+              {viewContent.map((element) => (
                 <div>
-                   
-                    {/* <img src="./public/profile.png" style={styles.image}></img> */}
-               
-                    {/* <TitleText>{state.title}</TitleText> */}
-                    <TitleText>
-                    {viewContent.map(element =>
-                        <div>
-                            {/* <div style={StyleSheet.imageContainer}> */}
-                            {/* <img src={require("./tino.png")} style={styles.image} /> */}
-                        {/* </div> */}
-                            <h2>{element.title}</h2>
-                            <div>
-                                {/* {element.content} */}
-                                {ReactHtmlParser(element.content)}
-                            </div>
-                        </div>
-                    )}
-                    </TitleText>
+                  {/* <div style={StyleSheet.imageContainer}> */}
+                  {/* <img src={require("./tino.png")} style={styles.image} /> */}
+                  {/* </div> */}
+                  <h2>{element.title}</h2>
+                  <div>
+                    {/* {element.content} */}
+                    {ReactHtmlParser(element.content)}
+                  </div>
                 </div>
-                </PostContainer>
-            </Container>
-        </Wrapper>
-    );
+              ))}
+            </TitleText>
+          </div>
+        </PostContainer>
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default PostWritePage;
