d5f2679e6a498fb3ed23392778d57e5b248c4f0b
지윤 <dlwldbs369@tukorea.ac.kr>
2023-05-29 13:34:28 +0900

    fix : array -> String , 소문자 -> 대문자
    - 백엔드로 Post 요청 시 language가 배열로 전송됨 따라, 문자열로 받는 백엔드에서는 오류가 발생했었음 -> 프론트에서 배열을 문자열로 바꿔 해결 
    - 언어 이름을 소문자에서 대문자로 수정

--- src/Manage_problem/create_problem.js
+++ src/Manage_problem/create_problem.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './problem.css';
-
+import "./problem.css";
 
 function ProblemForm() {
   const [problem, setProblem] = useState({
@@ -15,7 +14,7 @@ function ProblemForm() {
     language: [],
   });
 
-  const [selectedLevel, setSelectedLevel] = useState('');
+  const [selectedLevel, setSelectedLevel] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -60,7 +59,7 @@ function ProblemForm() {
       input_format: problem.input_format,
       output_format: problem.output_format,
       hint: problem.hint,
-      language: problem.language,
+      language: problem.language.toString(),
     };
     try {
       await axios.post(`http://127.0.0.1:8000/api/v1/problems/`, newProblem);
@@ -71,81 +70,163 @@ function ProblemForm() {
     }
   };
 
-
-
   return (
-<div className="container">
-	<form method="post" onSubmit={handleSubmit}>
-    <h3>Add Problem</h3><br />
-    <div className="container2">
-    <div>
-      <label htmlFor="level" className="level-label">level</label>
-      <select id="level"  name="level"
-      onChange={handleChange}
-      defaultValue=""
-      required>
-        <option value="" disabled hidden>문제 난이도</option>
-        <option value="1">level 1</option>
-        <option value="2">level 2</option>
-        <option value="3">level 3</option>
-        <option value="4">level 4</option>
-        <option value="5">level 5</option>
-      </select>
-    </div>
+    <div className="container">
+      <form method="post" onSubmit={handleSubmit}>
+        <h3>Add Problem</h3>
+        <br />
+        <div className="container2">
+          <div>
+            <label htmlFor="level" className="level-label">
+              level
+            </label>
+            <select
+              id="level"
+              name="level"
+              onChange={handleChange}
+              defaultValue=""
+              required
+            >
+              <option value="" disabled hidden>
+                문제 난이도
+              </option>
+              <option value="1">level 1</option>
+              <option value="2">level 2</option>
+              <option value="3">level 3</option>
+              <option value="4">level 4</option>
+              <option value="5">level 5</option>
+            </select>
+          </div>
 
-        <div>
-            <label htmlFor="title" >Title</label>
-            <textarea type="text" id="title" name="title" value={problem.title} placeholder="문제 제목" onChange={handleChange} required />
-        </div>
-        <div>
-            <label htmlFor="description" >Description</label>
-            <textarea type="text" id="description" name="description" value={problem.description}  placeholder="문제 설명" onChange={handleChange} required />
-        </div>
-        <div>
+          <div>
+            <label htmlFor="title">Title</label>
+            <textarea
+              type="text"
+              id="title"
+              name="title"
+              value={problem.title}
+              placeholder="문제 제목"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea
+              type="text"
+              id="description"
+              name="description"
+              value={problem.description}
+              placeholder="문제 설명"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
             <label htmlFor="input_format">Input Format</label>
-            <textarea type="text" id="input_format" name="input_format" value={problem.input_format}  placeholder="입력값" onChange={handleChange} required />
-        </div>
-        <div>
+            <textarea
+              type="text"
+              id="input_format"
+              name="input_format"
+              value={problem.input_format}
+              placeholder="입력값"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
             <label htmlFor="output_format">Output Format</label>
-            <textarea type="text" id="output_format" name="output_format" value={problem.output_format}  placeholder="출력값" onChange={handleChange} required />
-        </div>
-        <div>
+            <textarea
+              type="text"
+              id="output_format"
+              name="output_format"
+              value={problem.output_format}
+              placeholder="출력값"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
             <label htmlFor="hint">Hint</label>
-            <textarea type="text" id="hint" value={problem.hint} name="hint" placeholder="힌트" onChange={handleChange} required />
+            <textarea
+              type="text"
+              id="hint"
+              value={problem.hint}
+              name="hint"
+              placeholder="힌트"
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-    </div>
 
-    <div className="flex items-start mb-6">
-      <div>Language & Type</div>
-      <div className="language-checkbox">
-        <label title="GCC 7.5">
-        <input type="checkbox" name="language" value="c" checked={problem.language.includes('c')} onChange={handleCheckboxChange} /> C
-        </label>
-      <label title="G++ 7.5">
-      <input type="checkbox" name="language" value="c++" checked={problem.language.includes('c++')} onChange={handleCheckboxChange} /> C++
-      </label>
-      <label title="OpenJDK 1.8">
-      <input type="checkbox" name="language" value="java"  checked={problem.language.includes('java')} onChange={handleCheckboxChange}/> Java
-      </label>
-      <label title="Python 3.6">
-      <input type="checkbox" name="language" value="python3" checked={problem.language.includes('python3')} onChange={handleCheckboxChange}/> Python3
-      </label>
-      </div>
-      <label htmlFor="type" className="type"></label>
-      <select id="type"  name="type" defaultValue="" onChange={handleChange} required>
-        <option value="" disabled hidden>문제 유형</option>
-        <option value="code">코드 문제</option>
-        <option value="select">O / X 문제</option>
-        <option value="value">빈칸 문제</option>
-      </select>
-
+        <div className="flex items-start mb-6">
+          <div>Language & Type</div>
+          <div className="language-checkbox">
+            <label title="GCC 7.5">
+              <input
+                type="checkbox"
+                name="language"
+                value="C"
+                checked={problem.language.includes("C")}
+                onChange={handleCheckboxChange}
+              />{" "}
+              C
+            </label>
+            <label title="G++ 7.5">
+              <input
+                type="checkbox"
+                name="language"
+                value="C++"
+                checked={problem.language.includes("C++")}
+                onChange={handleCheckboxChange}
+              />{" "}
+              C++
+            </label>
+            <label title="OpenJDK 1.8">
+              <input
+                type="checkbox"
+                name="language"
+                value="JAVA"
+                checked={problem.language.includes("JAVA")}
+                onChange={handleCheckboxChange}
+              />{" "}
+              Java
+            </label>
+            <label title="Python 3.6">
+              <input
+                type="checkbox"
+                name="language"
+                value="Python3"
+                checked={problem.language.includes("Python3")}
+                onChange={handleCheckboxChange}
+              />{" "}
+              Python3
+            </label>
+          </div>
+          <label htmlFor="type" className="type"></label>
+          <select
+            id="type"
+            name="type"
+            defaultValue=""
+            onChange={handleChange}
+            required
+          >
+            <option value="" disabled hidden>
+              문제 유형
+            </option>
+            <option value="code">코드 문제</option>
+            <option value="select">O / X 문제</option>
+            <option value="value">빈칸 문제</option>
+          </select>
+        </div>
+        <div className="btn">
+          <button onClick={handleSubmit} type="button" className="submit-btn">
+            Save
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="btn">
-      <button onClick={handleSubmit} type="button" className="submit-btn">Save</button>
-    </div>
-
-</form>
-</div>
   );
 }
 
