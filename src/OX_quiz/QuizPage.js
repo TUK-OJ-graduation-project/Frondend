8faa190d268bcf1c6e23c81e45589fba37ebdd7e
goYoungOd <lyslys77@tukorea.ac.kr>
2023-06-22 16:32:48 +0900

    ox 퀴즈 문제 다음 버튼 수정

--- src/OX_quiz/QuizPage.js
+++ src/OX_quiz/QuizPage.js
@@ -1,8 +1,8 @@
-import './quiz.css';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import tino from './tino2.png';
+import "./quiz.css";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import tino from "./tino2.png";
 
 function QuizPage() {
   const [problemData, setProblemData] = useState({});
@@ -31,23 +31,23 @@ function QuizPage() {
   const [nextProblem, setNextProblem] = useState("");
 
   const loadNextProblem = () => {
-    const nextProblemId = parseInt(id) + 1;
+    const nextProblemId = parseInt(problemData.id) + 1;
     axios
       .get(`http://127.0.0.1:8000/api/v1/problems/select/${nextProblemId}/`)
-       .then(function (response) {
-      console.log(response);
-      setNextProblem(response.data);
-      setProblemData(response.data); // 문제 데이터 업데이트
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+      .then(function (response) {
+        console.log(response);
+        setNextProblem(response.data);
+        setProblemData(response.data); // 문제 데이터 업데이트
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const handleAnswerSubmit = (is_correct) => {
     setLoading(true);
     setIsCorrect(is_correct);
-    setUserAnswers(prevAnswers => [...prevAnswers, is_correct]);
+    setUserAnswers((prevAnswers) => [...prevAnswers, is_correct]);
     setFeedback(is_correct ? "정답입니다!" : "틀렸습니다.");
     setLoading(false);
   };
@@ -64,32 +64,38 @@ function QuizPage() {
 
   return (
     <div>
-    <div className='tino-image'>
-      <img src={tino} className='quiz-tino'/>
-    </div>
-    <div className="quiz-container">
-      <div className="qzproblem-container">
-        {problemData && Object.keys(problemData).length > 0 && (
-          <div key={problemData.id}>
-            <p className="qzproblem-container2">{problemData.title}</p>
-            <p className="qzproblem-container2">{problemData.description}</p>
-          </div>
-        )}
+      <div className="tino-image">
+        <img src={tino} className="quiz-tino" />
       </div>
-      <div className="answer-container">
-        <button className="answer-btn" onClick={() => handleAnswerSubmit(problemData.is_correct)}>
-          O
-        </button>
-        <button className="answer-btn" onClick={() => handleAnswerSubmit(!problemData.is_correct)}>
-          X
-        </button>
-      </div>
-      <p className="feedback">{feedback}</p>
-      {/* <div className="answer-container">
+      <div className="quiz-container">
+        <div className="qzproblem-container">
+          {problemData && Object.keys(problemData).length > 0 && (
+            <div key={problemData.id}>
+              <p className="qzproblem-container2">{problemData.title}</p>
+              <p className="qzproblem-container2">{problemData.description}</p>
+            </div>
+          )}
+        </div>
+        <div className="answer-container">
+          <button
+            className="answer-btn"
+            onClick={() => handleAnswerSubmit(problemData.is_correct)}
+          >
+            O
+          </button>
+          <button
+            className="answer-btn"
+            onClick={() => handleAnswerSubmit(!problemData.is_correct)}
+          >
+            X
+          </button>
+        </div>
+        <p className="feedback">{feedback}</p>
+        {/* <div className="answer-container">
       <button className="answer-btn" onClick={() => renderNextButton()}>다음</button>
       </div> */}
-      {renderNextButton()}
-    </div>
+        {renderNextButton()}
+      </div>
     </div>
   );
 }
